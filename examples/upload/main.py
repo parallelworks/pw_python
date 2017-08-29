70f005698802d836255f22dffafd5559943c65a0
Alex Hays <ahays@uchicago.edu>
2017-08-29 16:31:06 -0500

    examples: arg-ify upload example

--- examples/upload/main.py
+++ examples/upload/main.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
-from pw.uploader import Upload
+from argparse import ArgumentParser
 
-MB = 1024 * 1024
+from pw.uploader import Upload
+from pw.utils import BIN_SIZES
+
+
+def get_parser():
+    parser = ArgumentParser(description='upload a file to Parallel Works')
+    parser.add_argument('-f', '--file', help='path to file to upload', type=str)
+    parser.add_argument('-w', '--workspace', help='workspace id for uploaded file', type=str)
+    parser.add_argument('-k', '--key', help='API key to authenticate with', type=str)
+    parser.add_argument('-e', '--endpoint', help='endpoint to upload to', type=str)
+    parser.add_argument('-s', '--size', help='max read size of file', type=int, default=8*BIN_SIZES['M'])
+    parser.add_argument('-r', '--retry', help='max retries to send all failing chunks', type=int, default=15)
+    parser.add_argument('-t', '--timeout', help='time between retry attempts', type=int, default=5)
+
+    return parser
 
 
 def main():
-    filename = 'files/1mb.txt'
-    workspace_id = '<WORKSPACE ID>'  # Note: takes ID not workspace name
-    key = '<API KEY>'
-    max_read_size = 8 * MB
-    max_retries = 15
-    retry_timeout = 5
-    endpoint = 'https://go.parallel.works/api/upload'
+    p = get_parser()
+    args = vars(p.parse_args())
+
+    filename = args['file']
+    workspace_id = args['workspace']
+    key = args['key']
+    max_read_size = args['size']
+    max_retries = args['retry']
+    retry_timeout = args['timeout']
+    endpoint = args['endpoint']
 
     upload = Upload(filename, workspace_id, key, max_read_size, max_retries, retry_timeout)
     res = upload.submit(endpoint)
