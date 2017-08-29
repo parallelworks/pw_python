99c914cb1745074196594343e9429256e876ee43
Alex Hays <ahays@uchicago.edu>
2017-08-29 17:06:26 -0500

    benchmarks: fix gen files script

--- benchmarks/upload/gen_files.sh
+++ benchmarks/upload/gen_files.sh
@@ -6,17 +6,17 @@ mkdir $updir/files
 
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
-   fallocate -l 1K files/1K.txt
-   fallocate -l 1M files/1M.txt
-   fallocate -l 100M files/100M.txt
-   fallocate -l 2G files/2G.txt
-   fallocate -l 4G files/4G.txt
-   fallocate -l 10G files/10gG.txt
+   fallocate -l 1K $updir/files/1K.txt
+   fallocate -l 1M $updir/files/1M.txt
+   fallocate -l 100M $updir/files/100M.txt
+   fallocate -l 2G $updir/files/2G.txt
+   fallocate -l 4G $updir/files/4G.txt
+   fallocate -l 10G $updir/files/10G.txt
 elif [[ "$unamestr" == 'Darwin' ]]; then
-   mkfile 1K files/1K.txt
-   mkfile 1M files/1M.txt
-   mkfile 100M files/100M.txt
-   mkfile 2G files/2G.txt
-   mkfile 4G files/4G.txt
-   mkfile 10G files/10gG.txt
+   mkfile 1K $updir/files/1K.txt
+   mkfile 1M $updir/files/1M.txt
+   mkfile 100M $updir/files/100M.txt
+   mkfile 2G $updir/files/2G.txt
+   mkfile 4G $updir/files/4G.txt
+   mkfile 10G $updir/files/10G.txt
 fi
